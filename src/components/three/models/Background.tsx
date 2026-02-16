cd71436a74b6b863c7b8ea54d814689562b9017e
blackwalt608 <merteliacik@hotmail.com>
2026-02-16 19:09:40 +0300

    background scale

--- src/components/three/models/Background.tsx
+++ src/components/three/models/Background.tsx
@@ -2,7 +2,7 @@ import "../shaders/BackgroundMaterial";
 
 export default function Background() {
   return (
-    <mesh scale={[20, 5, 1]} position={[0, 2, -4]}>
+    <mesh scale={[50, 5, 1]} position={[0, 2, -4]}>
       <planeGeometry args={[1, 1]} />
       {/* @ts-ignore */}
       <backgroundMaterial />
