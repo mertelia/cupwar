3bc0243cb5213f2e579e2abc7498d12f7e1d959a
blackwalt608 <merteliacik@hotmail.com>
2026-02-16 21:52:15 +0300

    spamEdit

--- src/server/actions/sendScore.ts
+++ src/server/actions/sendScore.ts
@@ -4,25 +4,40 @@ import { createSafeActionClient } from "next-safe-action";
 import { ScoreSchema } from "../../../types/score-schema";
 import { db } from "..";
 import { scores } from "../schema";
-import { headers } from "next/headers";
+
+const cooldownMap = new Map<string, number>();
 
 const actionClient = createSafeActionClient();
+
 export const sendScore = actionClient
   .inputSchema(ScoreSchema)
   .action(async ({ parsedInput: { score, nick } }) => {
-    const headerList = await headers();
-    const country = headerList.get("x-vercel-ip-country") || "Unknown";
+    const headerList = await import("next/headers").then((m) => m.headers());
+    const ip =
+      headerList.get("x-forwarded-for") ??
+      headerList.get("x-real-ip") ??
+      "unknown";
+
+    const now = Date.now();
+    const lastSubmit = cooldownMap.get(ip);
+    if (lastSubmit && now - lastSubmit < 20000) {
+      return { error: "You can submit only once every 20 seconds." };
+    }
+
+    cooldownMap.set(ip, now);
 
     try {
       await db.insert(scores).values({
-        nick: nick,
-        score: score,
-        country: country,
+        nick,
+        score,
       });
 
-      return { success: "Saved!", country };
+      return {
+        success: "Your score saved!",
+        data: { score, nick },
+      };
     } catch (error) {
-      console.error(error);
+      console.error("An error occurred", error);
       return { error: "Something went wrong!" };
     }
   });
