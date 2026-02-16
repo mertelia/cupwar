aaf08c8ff97630339d02b8a258f69827e0f70e48
blackwalt608 <merteliacik@hotmail.com>
2026-02-16 17:48:16 +0300

    vercelDeploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${abcdFont.variable} antialiased `}>{children}</body>
+      <body className={`${abcdFont.variable} antialiased`}>{children}</body>
     </html>
   );
 }
